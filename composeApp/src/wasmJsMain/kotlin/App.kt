83e03f3bca2dc7254407423640504f9cfae14b97
Yamin Siahmargooei <yamin8000@yahoo.com>
2024-03-05 01:49:53 +0330

    init

---
+++ composeApp/src/wasmJsMain/kotlin/App.kt
@@ -0,0 +1,23 @@
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun App() {
+    MaterialTheme {
+        var showContent by remember { mutableStateOf(false) }
+        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+            Button(onClick = { showContent = !showContent }) {
+                Text("Click me!")
+            }
+            val greeting = remember { Greeting().greet() }
+            if (showContent)
+                Text("Compose: $greeting")
+        }
+    }
+}
